ac79ff9aa83367da2974dd444c81dec6e6c7fdb1
Guter Stiel <opi70@web.de>
2018-02-19 10:42:25 +0100

    first attempt

--- WS17/frename.sh
+++ WS17/frename.sh
@@ -1,1 +1,16 @@
-hallo123
+#!/bin/bash 
+# Hängt für alle Dateien im angegebenen Verzeichnis dir 
+#die Zeichenkette string an den aktuellen Dateinamen an 
+#(Umbenennung aller Dateien im Verzeichnis, Unterverzeichnisse bleiben unberührt). 
+# Thorben Schomacker
+# 19.02.2018
+
+echo Umbenennung im Verzeichnis: $1
+echo Durch anhaengen von $2 an den Dateinamen
+
+# Inhalt des aktuellen Arbeitsverzeichnisses als 
+#Liste von Dateinamen ausgeben. Übergebene 
+#Infos SPEC (Dateiname oder Verzeichnis) werden 
+#verwendet
+
+ls [-la] [SPEC]
