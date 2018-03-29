8d560a7516bc2d8a9688c7342cf9baad52e6e8a5
th0rben <opi70@web.de>
2018-03-29 12:38:37 +0200

    c file erstellt

---
+++ S18/hello.c
@@ -0,0 +1,12 @@
+/*
+ *      p3-1.c
+ *      Beispielprogramm 1, Abschnitt 3
+ */
+
+  void main(void)
+  {
+	/* creates a new text file */
+    fopen("textFile.txt" ,"a");
+	fclose(fp);
+
+  } /* main() */
