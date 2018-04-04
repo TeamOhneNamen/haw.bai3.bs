bb034662dc13d0e6d0f3d137719148e1c5ac3668
th0rben <opi70@web.de>
2018-04-04 15:07:50 +0200

    protokoll

--- C/mkfile.c
+++ C/mkfile.c
@@ -3,12 +3,20 @@
  *
  */
 #include <stdio.h>
-void main(void) {
-	char* filename = fgets(); 
-	#https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
+#include <string.h>
+#include <fcntl.h>
+int main(void) {
+	FILE *fp;
+	int size = 30;
+	char filename[size];
+	int mode = 0700;
+
+	fgets(filename, size, fp); 
+	//https://stackoverflow.com/questions/2693776/removing-trailing-newline-character-from-fgets-input
 	strtok(filename, "\n");
-	file = creat(*/filename.txt);
-	file.close();
+	creat(strcat(strcat("./", filename), ".txt"), mode);
+	fclose(fp);
 	printf("Name der neuen Datei: %s\n", filename);
 	printf("Die Datei bsp1 wurde erfolgreich angelegt!");
+	return(0);
 }
