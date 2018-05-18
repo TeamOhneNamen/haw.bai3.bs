a206fc588cccdb47cd3118b1c6e850fe5e685979
Sünktaktik-Schuggah <trendelenburger19.04@googlemail.com>
2018-05-18 17:08:14 +0200

    Mensa fertig

--- Java/src/SimMensa/Kasse.java
+++ Java/src/SimMensa/Kasse.java
@@ -1,36 +1,76 @@
 package SimMensa;
 
 import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class Kasse extends Thread  implements Comparable<Kasse>{
+public class Kasse extends Thread implements Comparable<Kasse> {
 
 	private int KASSENNUMMER;
-	public ArrayList<Student> studentenListe = new ArrayList<Student>(); 
-	
+	private int bezahlvorgaenge;
+	public ArrayList<Student> studentenListe = new ArrayList<Student>();
+	private boolean kasseOffen = true;
+
+	private Lock lock = new ReentrantLock();
+
+	public int getBezahlvorgaenge() {
+		return bezahlvorgaenge;
+	}
+
 	public Integer getStudentenListeSice() {
 		return studentenListe.size();
 	}
-	
+
 	public int getKASSENNUMMER() {
 		return KASSENNUMMER;
 	}
 
-	public void setKASSENNUMMER(int kassenNummer) {
-		KASSENNUMMER = kassenNummer;
+	public void setKASSENNUMMER(int kassennummer) {
+		KASSENNUMMER = kassennummer;
 	}
-	
+
 	@Override
-    public int compareTo(Kasse other) {
-    	return this.getStudentenListeSice().compareTo(other.getStudentenListeSice());
-    }
-	
-	public Kasse() {
+	public int compareTo(Kasse other) {
+		return this.getStudentenListeSice().compareTo(other.getStudentenListeSice());
 	}
-	
-	public void run(){
-		
+
+	public Kasse() {
+		bezahlvorgaenge = 0;
+	}
+
+	public void run() {
 		System.out.println("Kasse nummer: " + KASSENNUMMER + " ist jetzt Eröffnet");
 		
+		arbeiten();
 	}
 	
+	private void arbeiten() {
+		
+		while(kasseOffen) {
+			if (!studentenListe.isEmpty()) {
+				studentenListe.get(0).bezahlenAufruf();
+			}
+		}
+	}
+
+	public void bezahlen(int dauerBezahlen) {
+		lock.lock();
+		try {
+			Thread.sleep(dauerBezahlen);
+		} catch (InterruptedException e) {
+			this.interrupt();
+		}
+		bezahlvorgaenge++;
+		System.out.println("Student: " + studentenListe.get(0).getID() + " hat bezahlt");
+		studentenListe.get(0).essen();
+		studentenListe.remove(0);
+
+		lock.unlock();
+	}
+	@Override
+	public void interrupt() {
+		kasseOffen = false;
+		super.interrupt();
+	}
+
 }
--- Java/src/SimMensa/Main.java
+++ Java/src/SimMensa/Main.java
@@ -1,12 +1,30 @@
 package SimMensa;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Mensa mensa = new Mensa(3, 20);
 		mensa.start();
-
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		mensa.interrupt();
+		try {
+			mensa.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ArrayList<Kasse> kassenliste = mensa.getkassenListe();
+		for(int i = 0; i<kassenliste.size(); i++) {
+			System.out.println("an kasse " + kassenliste.get(i).getKASSENNUMMER() + " wurden " +kassenliste.get(i).getBezahlvorgaenge() + " Bezahlvorgänge abgeschlossen!");
+		}
 	}
 
 }
