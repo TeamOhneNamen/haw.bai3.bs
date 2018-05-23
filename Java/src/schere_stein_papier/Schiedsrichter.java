f89a24d8592d701d439d9c4ef03f24f2c28c782a
Guter Stiel <opi70@web.de>
2018-05-23 11:05:53 +0200

    per hand

--- Java/src/schere_stein_papier/Schiedsrichter.java
+++ Java/src/schere_stein_papier/Schiedsrichter.java
@@ -4,84 +4,26 @@ public class Schiedsrichter extends Thread {
 	private Tisch tisch;
 	private Spieler spieler_1;
 	private Spieler spieler_2;
-	int runde = 1;
-	boolean vorbei = false;
+	private int RUNDEN = 20;
 
-	public Schiedsrichter(Tisch t) {
+	public Schiedsrichter(Tisch t, Spieler s1, Spieler s2) {
 		this.tisch = t;
-		this.spieler_1 = this.tisch.getSpieler_1();
-		this.spieler_2 = this.tisch.getSpieler_2();
-		
+		this.spieler_1 = s1;
+		this.spieler_2 = s2;
+
 	}
-
+	@Override
 	public void run() {
-		
-		while (!vorbei) {
-
-			tisch.haendeAufDenTisch();
-			auswertung();
-			ausgabe();
-			tisch.leeren();
+		int runde = 1;
+		 while (runde <= RUNDEN) {
+//		while (true) {
+			System.out.println("round:  " + ((Integer) runde).toString());
+			System.out.println("Auswertung: ");
+			this.tisch.auswertung(this);
+			System.out.println(spieler_1.getSpielobjekt().toString() + " vs. " + spieler_2.getSpielobjekt().toString());
+			this.tisch.zugriff(this);
+			this.tisch.leeren();
 			runde++;
 		}
 	}
-	
-
-	private void auswertung() {
-		switch (tisch.getSpieler_1_spielobjekt()) {
-		case SCHERE:
-			switch (tisch.getSpieler_2_spielobjekt()) {
-			case SCHERE:
-				break;
-			case STEIN:
-				this.spieler_2.win();
-				break;
-			case PAPIER:
-				this.spieler_1.win();
-				break;
-			}
-			break;
-		case STEIN:
-			switch (tisch.getSpieler_2_spielobjekt()) {
-			case SCHERE:
-				this.spieler_1.win();
-				break;
-			case STEIN:
-				break;
-			case PAPIER:
-				this.spieler_2.win();
-				break;
-			}
-			break;
-		case PAPIER:
-			switch (tisch.getSpieler_2_spielobjekt()) {
-			case SCHERE:
-				this.spieler_2.win();
-				break;
-			case STEIN:
-				this.spieler_1.win();
-				break;
-			case PAPIER:
-				break;
-			}
-			break;
-		}
-
-	}
-	
-	private void ausgabe() {
-		System.out.println("-----------------");
-		System.out.println("round:  " + ((Integer)runde).toString());
-		System.out.println(tisch.getSpieler_1_spielobjekt().toString() + " vs. " + tisch.getSpieler_2_spielobjekt().toString());
-		System.out.println(((Integer) spieler_1.getPoints()).toString() + " : "
-				+ ((Integer) spieler_2.getPoints()).toString());
-	}
-	
-	
-	@Override
-	public void interrupt() {
-		vorbei = true;
-		super.interrupt();
-	}
-	
 }
